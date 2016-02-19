124514a1a02b6dcd57c7af6d9101f28ca2341791
Space <audun@nytroe.net>
2016-02-20 00:25:45 +0100

    Added tested script from CJS

--- work-in-progress/autofill/autofill.js
+++ work-in-progress/autofill/autofill.js
@@ -1,1 +1,3 @@
-
+document.addEventListener("DOMContentLoaded", function(event) { 
+  if(!!document.querySelector("input[name='postusername']")) {document.querySelector("input[name='postusername']").value=document.querySelector("#editpost > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2)").innerHTML.split(" <")[0];}
+});
