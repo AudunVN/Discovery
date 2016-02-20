6724486600a3036bb1831bfc74e776b1b9ed7821
Space <audun@nytroe.net>
2016-02-20 14:42:30 +0100

    added prefill function, filled out structure

--- space.js
+++ space.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+  spaceJSExec();
+});
 
+function spaceJSExec() {
+  prefillEditUsername();
+}
+
+/* function declarations start */
+
+function prefillEditUsername() {
+  /* prefills the moderator edit interface with the username of the currently logged in user.  */
+  if(!!document.querySelector("input[name='postusername']")) {
+    document.querySelector("input[name='postusername']").value = document.querySelector("#editpost > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2)").innerHTML.split(" <")[0];
+  }
+}
+
+/* function declarations end */
