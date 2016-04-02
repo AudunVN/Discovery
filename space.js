cdf913f2b624047c4d38f3c286f42c22faefac97
Space <audun@nytroe.net>
2016-04-02 14:44:40 +0200

    Loading CSS earlier.

--- space.js
+++ space.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 });
 
 function spaceJSExec() {
+	loadCSS();
 	bindHelperFunctions();
 	prefillEditUsername();
 	resizeHandler();
-	loadCSS();
 	tempNavbarEdits();
 }
 
